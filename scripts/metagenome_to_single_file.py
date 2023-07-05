9f27ec1b545f13901deba32f33cf1f0f6a390d30
elieling <48514398+elieling@users.noreply.github.com>
2023-07-05 14:50:37 +0300

    Bugs fixed

--- scripts/metagenome_to_single_file.py
+++ scripts/metagenome_to_single_file.py
@@ -4,17 +4,11 @@ import pandas as pd
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-
 abundance_df = pd.read_csv(snakemake.input.abundances, sep='\t')
-logger.info(abundance_df)
-logger.info("len %s", len(abundance_df))
 n_rows = abundance_df.shape[0]
 
 for i in range(n_rows):
-    percentage = abundance_df.iloc[i]
+    percentage = abundance_df.iloc[i, 1]
     n_occurences = round(percentage)
     with open(snakemake.input.references[i], 'r') as source:
         with open(snakemake.output.report, 'a') as destination:
