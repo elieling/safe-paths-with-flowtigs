48996985b9ea78ab27cb45910d0e523cc8abb3c3
elieling <48514398+elieling@users.noreply.github.com>
2023-07-28 17:24:07 +0300

    Additional script completed

--- additional_scripts/Readme.md
+++ additional_scripts/Readme.md
@@ -1,1 +1,29 @@
 Additional software to discover the contig length of unaligned contig.
+
+Calculates the number of contigs with length greater than x and tries to match that number to the number of unaligned contigs.
+
+Results:
+    - Unitigs, unaligned contigs: 1314
+        > 44 : 1324
+        > 45 : 1311
+        NO EXACT MATCH!
+
+    - Trivial omnitigs, unaligned contigs: 135
+        > 11079 : 135
+           ...
+        > 11088 : 135
+
+    - multi-safe, unaligned contigs: 137
+        > 11000 : 137
+           ...
+        > 11049 : 137
+
+    - flowtigs, unaligned contigs: 241
+        > 4838 : 241
+           ...
+        > 4859 : 241
+
+    - omnitigs, unaligned contigs: 133
+        > 11240 : 133
+           ...
+        > 11268 : 133
--- additional_scripts/find_a.py
+++ additional_scripts/find_a.py
@@ -14,18 +14,108 @@ for sequence in unitigs:
     unitigs_arr.append(len(s_data))
 unitigs_n = np.array(unitigs_arr)
 
+gsum = -1
 for i in range(30, 5000000):
-    print(i)
     g = unitigs_n > i
+    gsum_early = gsum
     gsum = g.sum()
-    print(gsum)
     if gsum == 1314:
         print(i)
     elif gsum < 1314:
         break
-    if (1==100):
-        print(i, gsum)
-    if (1==1000):
-        print(i, gsum)
-    if (1==10000):
-        print(i, gsum)
+print("unitigs 1314")
+print(i-1, gsum_early)
+print(i, gsum)
+print("#"*70)
+
+
+unitigs = [i for i in SeqIO.parse("/home/ebingerv/VSC/snakemake-flowtigs/additional_scripts/data/flowtigs.fasta", 'fasta')]
+unitigs_arr = []
+for sequence in unitigs:
+    data = sequence.seq
+    s_data = str(data)
+    unitigs_arr.append(len(s_data))
+unitigs_n = np.array(unitigs_arr)
+
+gsum = -1
+for i in range(30, 5000000):
+    g = unitigs_n > i
+    gsum_early = gsum
+    gsum = g.sum()
+    if gsum == 241:
+        print("exact", i, gsum)
+    elif gsum < 241:
+        break
+print("flowtigs 241")
+print(i-1, gsum_early)
+print(i, gsum)
+print("#"*70)
+
+
+unitigs = [i for i in SeqIO.parse("/home/ebingerv/VSC/snakemake-flowtigs/additional_scripts/data/trivial-omnitigs.fasta", 'fasta')]
+unitigs_arr = []
+for sequence in unitigs:
+    data = sequence.seq
+    s_data = str(data)
+    unitigs_arr.append(len(s_data))
+unitigs_n = np.array(unitigs_arr)
+
+gsum = -1
+for i in range(30, 5000000):
+    g = unitigs_n > i
+    gsum_early = gsum
+    gsum = g.sum()
+    if gsum == 135:
+        print("exact", i, gsum)
+    elif gsum < 135:
+        break
+print("trivial omnitigs 135")
+print(i-1, gsum_early)
+print(i, gsum)
+print("#"*70)
+
+
+unitigs = [i for i in SeqIO.parse("/home/ebingerv/VSC/snakemake-flowtigs/additional_scripts/data/multi-safe.fasta", 'fasta')]
+unitigs_arr = []
+for sequence in unitigs:
+    data = sequence.seq
+    s_data = str(data)
+    unitigs_arr.append(len(s_data))
+unitigs_n = np.array(unitigs_arr)
+
+gsum = -1
+for i in range(30, 5000000):
+    g = unitigs_n > i
+    gsum_early = gsum
+    gsum = g.sum()
+    if gsum == 137:
+        print("exact", i, gsum)
+    elif gsum < 137:
+        break
+print("muti-safe 137")
+print(i-1, gsum_early)
+print(i, gsum)
+print("#"*70)
+
+
+unitigs = [i for i in SeqIO.parse("/home/ebingerv/VSC/snakemake-flowtigs/additional_scripts/data/omnitigs.fasta", 'fasta')]
+unitigs_arr = []
+for sequence in unitigs:
+    data = sequence.seq
+    s_data = str(data)
+    unitigs_arr.append(len(s_data))
+unitigs_n = np.array(unitigs_arr)
+
+gsum = -1
+for i in range(30, 5000000):
+    g = unitigs_n > i
+    gsum_early = gsum
+    gsum = g.sum()
+    if gsum == 133:
+        print("exact", i, gsum)
+    elif gsum < 133:
+        break
+print("omnitigs 133")
+print(i-1, gsum_early)
+print(i, gsum)
+print("#"*70)
