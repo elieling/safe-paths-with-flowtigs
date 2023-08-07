ce5c9b835ef30dfc819170c0b28f664379bc7cf4
elieling <48514398+elieling@users.noreply.github.com>
2023-08-07 16:42:44 +0300

    Bug fixes

--- additional_scripts/print_fasta_files_in_right_order.py
+++ additional_scripts/print_fasta_files_in_right_order.py
@@ -1,6 +1,10 @@
 import sys
 import pandas as pd
 
+
+# Algorithm to get the collection of fasta files for metagenomes
+
+
 folder_name = sys.argv[1]
 abundances_file = sys.argv[2]
 
@@ -12,6 +16,8 @@ first = True
 for name in names:
     if name.endswith(".fna") or name.endswith(".fasta"):
         if not first: result = result + ", "
+        new_name = name
+        if new_name.endswith(".fna"): new_name = new_name[:-4]
         result = result + "os.path.join(DATADIR, '" + folder_name + "', '" + name + "')"
         first = False
 
