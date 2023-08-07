e251f5042d45754dbea77ef943cc735978b693a8
elieling <48514398+elieling@users.noreply.github.com>
2023-08-07 15:27:40 +0300

    Additional script to get input folder name for metagenome

---
+++ additional_scripts/print_fasta_files_in_right_order.py
@@ -0,0 +1,20 @@
+import sys
+import pandas as pd
+
+folder_name = sys.argv[1]
+abundances_file = sys.argv[2]
+
+abundances_df = pd.reaad_csv(abundances_file, sep='\t')
+names = abundances_df["Size"]
+
+result = "["
+first = True
+for name in names:
+    if name.endswith(".fna") or name.endswith(".fasta"):
+        if not first: result = result + ", "
+        result = result + "os.path.join(DATADIR, '" + folder_name + "')"
+        first = False
+
+result = result + "]"
+
+print(result)
